2676fa17caed255d74a0fd545446c4af1796d96c
CherylNg <61680146+CherylNg@users.noreply.github.com>
2020-05-15 12:14:59 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,12 @@ basic.forever(function () {
     } else if (input.buttonIsPressed(Button.B)) {
         edubitMotors.setServoPosition(ServoChannel.S1, 150)
     } else {
-        edubitMotors.setServoPosition(ServoChannel.S1, 90)
+        edubitMotors.setServoPosition(ServoChannel.S1, pins.map(
+        edubitPotentioBit.readPotValue(),
+        0,
+        1023,
+        30,
+        150
+        ))
     }
 })
